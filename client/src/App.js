21a143f20d35957eb53dd46ba9d66510568e055e
Sanyam Dogra <sdlord07@gmail.com>
2020-01-14 01:20:44 +0530

    Added data provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Admin, Resource, ListGuesser } from "react-admin";
-import simpleRestProvider from "ra-data-simple-rest";
 import { createMuiTheme } from "@material-ui/core/styles";
+import jsonServerProvider from "ra-data-json-server";
 
 import AdminDashboard from "./components/AdminDashboard/AdminDashboard";
 import CustomLoginPage from "./components/LoginPage/LoginPage";
@@ -9,7 +9,7 @@ import CustomLoginPage from "./components/LoginPage/LoginPage";
 import "./App.css";
 import authProvider from "./authProvider";
 
-const dataProvider = simpleRestProvider("https://domain.tld/api");
+const dataProvider = jsonServerProvider("https://grievance-app.herokuapp.com");
 const theme = createMuiTheme({
   palette: {
     type: "dark" // Switching the dark mode on is a single property value change.
@@ -22,14 +22,14 @@ const theme = createMuiTheme({
 function App() {
   return (
     <Admin
-      title="Grievences Admin Portal"
+      title="Grievances Admin Portal"
       dataProvider={dataProvider}
       theme={theme}
       loginPage={CustomLoginPage}
       authProvider={authProvider}
       dashboard={AdminDashboard}
     >
-      <Resource name="Grievences" list={ListGuesser} />
+      <Resource name="users" list={ListGuesser} />
       <Resource name="Departments" list={ListGuesser} />
     </Admin>
   );
